fd655aa6463a43443e7866d0c24949eefc4558ad
Mohammed Affan <affanv14@gmail.com>
2019-05-04 16:00:21 -0400

    fix async issue

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -5,22 +5,16 @@ const router = express.Router();
 
 // Display the dashboard page
 router.get("/", (req, res) => {
-
-    // TODO Fix the asynchronicity problem. Speak to Abhinav
     const workspaces = [];
-    getWorkspaces(req.user.id).then(workspaces => workspaceList)
-        .then(console.log("Outside function."))
-        .then(console.log(workspaces))
-        .then(res.render("dashboard", {"workspaceList" : workspaces}));
+    getWorkspaces(req.user.id, res);
 });
 
-async function getWorkspaces(uid) {
-    let workspaceList = [];
-    await global.db.query(`SELECT w.wid, wname, wcreatorid, wtimestamp from WorkspaceUser wuser join Workspace w
+async function getWorkspaces(uid, res) {
+    query = global.db.query(`SELECT w.wid, wname, wcreatorid, wtimestamp from WorkspaceUser wuser join Workspace w
     on wuser.wid = w.wid where wuser.uid = ?`, uid, function(err, results, fields) {
         console.log(results);
-        workspaceList = JSON.parse(JSON.stringify(results));
-        return workspaceList;
+        val_list = JSON.parse(JSON.stringify(results));
+        res.render("dashboard", {"workspaceList" : val_list});
     });
 }
 
