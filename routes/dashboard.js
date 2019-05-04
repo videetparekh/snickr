8acbe97b4185345c381c1bee6baa5fb806b076cd
Mohammed Affan <affanv14@gmail.com>
2019-05-04 18:00:03 -0400

    add functionality to add workspaces

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -8,18 +8,61 @@ router.get("/", (req, res) => {
     const workspaces = [];
     getWorkspaces(req.user.id, res);
 });
+router.post("/addworkspace", (req, res) => {
+    addWorkspace(req.body.workspace_name, req.user.id, res);
+});
 
-async function getWorkspaces(uid, res) {
-    query = global.db.query(`SELECT w.wid, wname, wcreatorid, wtimestamp from WorkspaceUser wuser join Workspace w
-    on wuser.wid = w.wid where wuser.uid = ?`, uid, function(err, results, fields) {
-        console.log(results);
-        val_list = JSON.parse(JSON.stringify(results));
-        res.render("dashboard", {"workspaceList" : val_list});
+
+function addWorkspace(w_name, uid, res) {
+    global.db.getConnection(function (err, connection) {
+        if (err) throw err; // not connected!
+        console.log('here!');
+        connection.beginTransaction(function (err) {
+            if (err) { throw err; }
+            connection.query('Insert into Workspace(wname, wcreatorid, wtimestamp) values (?, ?, now())',
+                [w_name, uid], function (err, result) {
+                    if (err) {
+                        connection.rollback(function () {
+                            throw err;
+                        });
+                    }
+
+                    console.log('success!');
+                    connection.query('Insert into WorkspaceUser values(LAST_INSERT_ID(), ?, ?, now())', [uid, 'ADMINI'],
+                        function (err, result) {
+                            if (err) {
+                                console.log(err)
+                                connection.rollback(function () {
+                                    throw err;
+                                });
+                            }
+                            connection.commit(function (err) {
+                                if (err) {
+                                    connection.rollback(function () {
+                                        throw err;
+                                    });
+                                }
+                            });
+                            connection.release()
+                            getWorkspaces(uid, res);
+
+                        });
+                });
+        });
     });
 }
 
+async function getWorkspaces(uid, res) {
+    query = global.db.query(`SELECT w.wid, wname, wcreatorid, wtimestamp from WorkspaceUser wuser join Workspace w
+    on wuser.wid = w.wid where wuser.uid = ?`, uid, function (err, results, fields) {
+            console.log(results);
+            val_list = JSON.parse(JSON.stringify(results));
+            res.render("dashboard", { "workspaceList": val_list });
+        });
+}
+
 router.get("/test", (req, res) => {
-  res.render("test");
+    res.render("test");
 });
 
 module.exports = router;
