5f66a3e7a1abf0d574d325fc401e0c2f44fa51f5
Mohammed Affan <affanv14@gmail.com>
2019-05-04 21:00:10 -0400

    change callbacks to promises

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -6,56 +6,66 @@ const router = express.Router();
 // Display the dashboard page
 router.get("/", (req, res) => {
     const workspaces = [];
-    getWorkspaces(req.user.id, res);
+    getWorkspaces(req.user.id)
+    .then(val_list=>res.render("dashboard", { "workspaceList": val_list }));
 });
 router.post("/addworkspace", (req, res) => {
-    addWorkspace(req.body.workspace_name, req.user.id, res);
+    addWorkspace(req.body.workspace_name, req.user.id)
+    .then(values=>res.render("dashboard", { "workspaceList": values }));
 });
 
 
-function addWorkspace(w_name, uid, res) {
-    global.db.getConnection(function (err, connection) {
-        if (err) throw err;
-        connection.beginTransaction(function (err) {
-            if (err) { throw err; }
-            connection.query('Insert into Workspace(wname, wcreatorid, wtimestamp) values (?, ?, now())',
-                [w_name, uid], function (err, result) {
-                    if (err) {
-                        connection.rollback(function () {
-                            throw err;
-                        });
-                    }
-                    connection.query('Insert into WorkspaceUser values(LAST_INSERT_ID(), ?, ?, now())', [uid, 'ADMINI'],
-                        function (err, result) {
-                            if (err) {
-                                console.log(err)
-                                connection.rollback(function () {
-                                    throw err;
-                                });
-                            }
-                            connection.commit(function (err) {
+async function addWorkspace(w_name, uid) {
+    return new Promise((resolve, reject)=>{
+        global.db.getConnection(function (err, connection) {
+            if (err) throw err;
+            connection.beginTransaction(function (err) {
+                if (err) { throw err; }
+                connection.query('Insert into Workspace(wname, wcreatorid, wtimestamp) values (?, ?, now())',
+                    [w_name, uid], function (err, result) {
+                        if (err) {
+                            connection.rollback(function () {
+                                throw err;
+                            });
+                        }
+                        connection.query('Insert into WorkspaceUser values(LAST_INSERT_ID(), ?, ?, now())', [uid, 'ADMINI'],
+                            function (err, result) {
                                 if (err) {
+                                    console.log(err)
                                     connection.rollback(function () {
                                         throw err;
                                     });
                                 }
-                            });
-                            connection.release()
-                            getWorkspaces(uid, res);
+                                connection.commit(function (err) {
+                                    if (err) {
+                                        connection.rollback(function () {
+                                            throw err;
+                                        });
+                                    }
+                                });
+                                connection.release()
+                                if(err)
+                                    reject(err);
+                                getWorkspaces(uid).then(value=>resolve(value));
 
-                        });
-                });
+                            });
+                    });
+            });
         });
     });
 }
 
-async function getWorkspaces(uid, res) {
+async function getWorkspaces(uid) {
+    return new Promise((resolve, reject)=>{
     query = global.db.query(`SELECT w.wid, wname, wcreatorid, wtimestamp from WorkspaceUser wuser join Workspace w
     on wuser.wid = w.wid where wuser.uid = ?`, uid, function (err, results, fields) {
-            console.log(results);
+            if(err)
+                reject(err);
             val_list = JSON.parse(JSON.stringify(results));
-            res.render("dashboard", { "workspaceList": val_list });
+            resolve(val_list);
         });
+
+    });
 }
 
 router.get("/test", (req, res) => {
