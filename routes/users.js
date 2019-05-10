3ec2cce253094ceaa85321886814d28ac3b3224a
videetparekh <videetparekh0047@gmail.com>
2019-05-10 14:10:42 -0400

    Changed user updates to callback

--- routes/users.js
+++ routes/users.js
@@ -10,7 +10,31 @@ router.get("/logout", (req, res) => {
 });
 
 router.get("/callback", (req, res) => {
-  
+    db.query('SELECT uid from SnickrUser where uid=?', req.user.id, function(err, results, fields) {
+      if (!results[0]) {
+        db.query('INSERT INTO SnickrUser(uid, name, email, nickname, joindate, lastlogin) VALUES (?,?,?,?,?,?)',
+        [
+          req.user.id,
+          req.user.profile.firstName+' '+req.user.profile.lastName,
+          req.user.profile.email,
+          req.user.profile.secondEmail,
+          new Date(req.user.created).toISOString().slice(0, 19).replace('T', ' '),
+          new Date(req.user.lastLogin).toISOString().slice(0, 19).replace('T', ' ')
+        ],
+       function(err, results, fields) {
+         if(err) {
+           console.log(err);
+         }
+       });
+     } else {
+       db.query('UPDATE SnickrUser SET lastlogin = ? WHERE uid = ?', [new Date(req.user.lastLogin).toISOString().slice(0, 19).replace('T', ' '), req.user.id],
+       function(err, results, fields){
+         if(err) {
+           console.log(err);
+         }
+       });
+     }
+   });
 });
 
 module.exports = router;
